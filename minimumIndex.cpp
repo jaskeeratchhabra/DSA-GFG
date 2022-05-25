a4b24e9208c3e6c85bb29d181c6d0aaf4a899209
jaskeeratchhabra <jkchhabra99@gmail.com>
2022-05-25 10:39:25 +0530

    push

---
+++ minimumIndex.cpp
@@ -0,0 +1,56 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+        
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int arr[], int n) 
+    { 
+        // Your code here
+        int m=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=n-1;j>=i;j--)
+            {
+                if(arr[j]>=arr[i])
+                {
+                    m=max(m,j-i);
+                    break;
+                }
+            }
+        }
+        return m;
+    }
+};
+
+// { Driver Code Starts.
+  
+/* Driver program to test above functions */
+int main() 
+{
+    int T;
+    //testcases
+    cin>>T;
+    while(T--){
+        int num;
+        //size of array
+        cin>>num;
+        int arr[num];
+        
+        //inserting elements
+        for (int i = 0; i<num; i++)
+            cin>>arr[i];
+        Solution ob;
+        
+        //calling maxIndexDiff() function
+        cout<<ob.maxIndexDiff(arr, num)<<endl;    
+        
+    }
+    return 0;
+}   // } Driver Code Ends
